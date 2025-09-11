002eb91a8e9f10711f51fdb3b103838b311e059d
Boris Hermann NANA <110834576+rmannnana@users.noreply.github.com>
2025-09-11 22:07:32 +0000

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mon-terraform-state-bucket"
-    key            = "infrastructure/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
-  }
-}
